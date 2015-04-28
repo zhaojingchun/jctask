77fcf761842552e3d188e182a7cf8dfd4c301cff
zhaojingchun521@163.com <zhaojingchun521@163.com>
2015-04-28 08:04:57 +0800

    task

---
+++ ctask/src/main/java/com/jc/service/task/DelUnderPathFiles.java
@@ -0,0 +1,7 @@
+package com.jc.service.task;
+
+/**
+ * Created by jingchun on 2015/4/27.
+ */
+public interface DelUnderPathFiles {
+}
---
+++ ctask/src/main/java/com/jc/service/task/impl/DelUnderPathFilesImpl.java
@@ -0,0 +1,10 @@
+package com.jc.service.task.impl;
+
+import com.jc.service.task.DelUnderPathFiles;
+
+/**
+ * Created by jingchun on 2015/4/27.
+ */
+public class DelUnderPathFilesImpl implements DelUnderPathFiles {
+
+}
