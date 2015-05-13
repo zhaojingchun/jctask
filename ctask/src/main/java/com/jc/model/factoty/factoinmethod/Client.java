70f9682d5f19773e508b055230e42c7ed267a241
zhaojingchun <zhaojingchun521@163.com>
2015-05-13 21:04:11 +0800

    回家

---
+++ ctask/src/main/java/com/jc/model/factoty/factoinmethod/Client.java
@@ -0,0 +1,15 @@
+package com.jc.model.factoty.factoinmethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhaojingchun
+ * Date: 15-5-13
+ * Time: 下午9:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+}
+
+class Creator{
+    public
+}
