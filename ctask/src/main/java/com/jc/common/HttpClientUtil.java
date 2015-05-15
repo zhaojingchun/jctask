7efe76335230a604c0f03e831f27e01ea3533189
zhaojingchun <zhaojingchun521@163.com>
2015-05-15 19:06:25 +0800

    add

--- ctask/src/main/java/com/jc/common/HttpClientUtil.java
+++ ctask/src/main/java/com/jc/common/HttpClientUtil.java
@@ -6,9 +6,11 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.params.ClientPNames;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
@@ -23,6 +25,12 @@ import java.util.List;
  */
 @Component
 public class HttpClientUtil {
+    private static Integer CONNECTION_TIMEOUT = 2 * 1000;    //设置请求超时2秒钟  根据业务调整
+    private static Integer SO_TIMEOUT = 2 * 1000;        //设置等待数据超时时间2秒钟 根据业务调整
+    //定义了当从ClientConnectionManager中检索ManagedClientConnection实例时使用的毫秒级的超时时间
+    //这个参数期望得到一个java.lang.Long类型的值。如果这个参数没有被设置，默认等于CONNECTION_TIMEOUT，因此一定要设置
+    private static Long CONN_MANAGER_TIMEOUT = 500L;  //该值就是连接不够用的时候等待超时时间，一定要设置，而且不能太大  ()
+
     private Logger log = Logger.getLogger(HttpClientUtil.class);
     @Resource
     private String charSet="";
@@ -33,10 +41,12 @@ public class HttpClientUtil {
         HttpPost httpPost = new HttpPost(url);
         //放头部信息
         httpPost.setHeader("Referer", "http://365.good7000.com/xz/cn/");
+//        postMethod.getParams().setParameter(HttpMethodParams.SO_TIMEOUT, soTimeOut);
         // 创建参数队列
         List<NameValuePair> formparams = new ArrayList<NameValuePair>();
         formparams.add(new BasicNameValuePair("username", "admin"));
         formparams.add(new BasicNameValuePair("password", "123456"));
+
         UrlEncodedFormEntity uefEntity;
         log.debug("executing request " + httpPost.getURI());
         CloseableHttpResponse response = null;
@@ -114,4 +124,24 @@ public class HttpClientUtil {
     public void setCharSet(String charSet) {
         this.charSet = charSet;
     }
+
+
+//    HttpConnectionManagerParams params = new HttpConnectionManagerParams();
+//    params.setConnectionTimeout(2000);
+//    params.setSoTimeout(2000);
+//    // 最大连接数
+//    params.setMaxTotalConnections(500);
+//    params.setDefaultMaxConnectionsPerHost(500);
+//    params.setStaleCheckingEnabled(true);
+//    connectionManager.setParams(params);
+//
+//    HttpClientParams httpClientParams = new HttpClientParams();
+//    // 设置httpClient的连接超时，对连接管理器设置的连接超时是无用的
+//    httpClientParams.setConnectionManagerTimeout(5000); //等价于4.2.3中的CONN_MANAGER_TIMEOUT
+//    httpClient = new HttpClient(connectionManager);
+//    httpClient.setParams(httpClientParams);
+//
+//    //另外设置http client的重试次数，默认是3次；当前是禁用掉（如果项目量不到，这个默认即可）
+//    httpClientParams.setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(0, false));
+
 }
---
+++ ctask/src/main/java/com/jc/model/observer/Client.java
@@ -0,0 +1,14 @@
+package com.jc.model.observer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhaojingchun
+ * Date: 15-5-15
+ * Time: 下午6:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client {
+}
+interface Observer{
+    public void update();
+}
