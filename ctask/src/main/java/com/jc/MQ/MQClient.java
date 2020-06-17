3151cc12a070083b3742b76adb7b980f22b4f911
zhaojingchun <zhaojingchun@jd.com>
2020-06-17 21:40:43 +0800

    add

--- ctask/src/main/java/com/jc/MQ/MQClient.java
+++
@@ -1,6 +0,0 @@
-package com.jc.MQ;
-
-
-public class MQClient {
-
-}
--- ctask/src/main/java/com/jc/service/task/DelUnderPathFiles.java
+++
@@ -1,8 +0,0 @@
-package com.jc.service.task;
-
-/**
- * Created by jingchun on 2015/4/27.
- */
-public interface DelUnderPathFiles {
-    void delAllPathFiles();
-}
